fe384c7ab7bd05ed7d8e0940304dfe41d434529b
hackerudroids <tomerjava@gmail.com>
2017-06-05 20:15:36 +0300

    finished with AsyncTask

--- app/src/main/java/hackeru/edu/parsingxml/CurrencyDataSource.java
+++ app/src/main/java/hackeru/edu/parsingxml/CurrencyDataSource.java
@@ -1,9 +1,14 @@
 package hackeru.edu.parsingxml;
 
 import android.os.AsyncTask;
-import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,30 +16,51 @@ import java.util.List;
  */
 
 public class CurrencyDataSource {
+    public interface OnCurrenciesArrivedListener{
+            void onCurrenciesArrived(List<Currency> currencies);
+    }
 
-    public static void getCurrencies(){
-        //http://www.boi.org.il/currency.xml
+    public static void getCurrencies(final OnCurrenciesArrivedListener listener) {
         AsyncTask<String, Integer, List<Currency>> asyncTask = new AsyncTask<String, Integer, List<Currency>>() {
             @Override
             protected List<Currency> doInBackground(String... params) {
                 //code that runs in the background
                 try {
                     String xml = IO.readWebSite("http://www.boi.org.il/currency.xml");
-                    Log.d("Hackeru", xml);
+                    List<Currency> currencies = parse(xml);
+                    return currencies;
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 return null;
             }
-
             @Override
             protected void onPostExecute(List<Currency> currencies) {
                 //code that runs on the ui thread
+                listener.onCurrenciesArrived(currencies);
             }
         };
-
         asyncTask.execute();
     }
+
+    private static List<Currency> parse(String xml) {
+        List<Currency> currencies = new ArrayList<>();
+        Document document = Jsoup.parse(xml);
+        Elements elements = document.getElementsByTag("CURRENCY");
+
+        for (Element element : elements) {
+            String name = element.getElementsByTag("NAME").first().text();
+            int unit = Integer.parseInt(element.getElementsByTag("UNIT").first().text());
+            String currencyCode = element.getElementsByTag("CURRENCYCODE").first().text();
+            String country = element.getElementsByTag("COUNTRY").first().text();
+            Double rate = Double.valueOf(element.getElementsByTag("RATE").first().text());
+            Double change = Double.valueOf(element.getElementsByTag("CHANGE").first().text());
+
+            currencies.add(new Currency(name, unit, currencyCode, country, rate, change));
+        }
+        return currencies;
+    }
+
 
     //inner class POJO
     public static class Currency {
@@ -44,6 +70,7 @@ public class CurrencyDataSource {
         private final String country;
         private final double rate;
         private final double change;
+
         //Constructor
         public Currency(String name, int unit, String currencyCode, String country, double rate, double change) {
             this.name = name;
@@ -53,25 +80,32 @@ public class CurrencyDataSource {
             this.rate = rate;
             this.change = change;
         }
+
         //Getters
         public String getName() {
             return name;
         }
+
         public int getUnit() {
             return unit;
         }
+
         public String getCurrencyCode() {
             return currencyCode;
         }
+
         public String getCountry() {
             return country;
         }
+
         public double getRate() {
             return rate;
         }
+
         public double getChange() {
             return change;
         }
+
         //toString
         @Override
         public String toString() {
--- app/src/main/java/hackeru/edu/parsingxml/CurrencyFragment.java
+++ app/src/main/java/hackeru/edu/parsingxml/CurrencyFragment.java
@@ -6,12 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CurrencyFragment extends Fragment {
+public class CurrencyFragment extends Fragment implements CurrencyDataSource.OnCurrenciesArrivedListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,11 +24,19 @@ public class CurrencyFragment extends Fragment {
         //v.findViewById
 
 
-
-        CurrencyDataSource.getCurrencies();
+        CurrencyDataSource.getCurrencies(this);
 
 
         return v;
     }
 
+    @Override
+    public void onCurrenciesArrived(List<CurrencyDataSource.Currency> currencies) {
+        if (currencies != null) {
+            Toast.makeText(getActivity(), currencies.toString(), Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getActivity(), "Error connecting to server", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
