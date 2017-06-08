5b38963b222bab7f71ff9af4123d3c91adf9c7e5
hackerudroids <hackerudroids@gmail.com>
2017-06-08 18:08:19 +0300

    ynet item and viewholder

--- app/src/main/java/hackeru/edu/parsingxml/YnetFragment.java
+++ app/src/main/java/hackeru/edu/parsingxml/YnetFragment.java
@@ -3,9 +3,12 @@ package hackeru.edu.parsingxml;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -33,4 +36,20 @@ public class YnetFragment extends Fragment implements YnetDataSource.OnYnetArriv
     }
 
 
+
+
+    static class YnetAdapter {
+
+        class YnetViewHolder extends RecyclerView.ViewHolder{
+            TextView tvTitle;
+            TextView tvDescription;
+            ImageView ivThumbnail;
+            public YnetViewHolder(View itemView) {
+                super(itemView);
+                tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+                tvDescription = (TextView) itemView.findViewById(R.id.tvDescription);
+                ivThumbnail = (ImageView) itemView.findViewById(R.id.ivThumbnail);
+            }
+        }
+    }
 }
