613a037c82ef2d846cc67f3e0f42d881f507b84e
hackerudroids <hackerudroids@gmail.com>
2017-06-08 20:55:14 +0300

    webview configuration

--- app/src/main/java/hackeru/edu/parsingxml/YnetArticleFragment.java
+++ app/src/main/java/hackeru/edu/parsingxml/YnetArticleFragment.java
@@ -1,12 +1,17 @@
 package hackeru.edu.parsingxml;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 /**
@@ -30,7 +35,34 @@ public class YnetArticleFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_ynet_article, container, false);
         webView = (WebView) v.findViewById(R.id.webView);
+
+        config(webView);
+
         webView.loadUrl(link);
         return v;
+    }
+
+    private void config(final WebView webView) {
+        //Enable JavaScript
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient(){
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);
+                return true;
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                webView.loadUrl(url);
+                return true;
+            }
+        });
     }
 }
