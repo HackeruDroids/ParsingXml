d415d6ca33e060d6d143c77acbf110d7d452064d
hackerudroids <hackerudroids@gmail.com>
2017-06-08 18:30:04 +0300

    Ynet Adapter

--- app/src/main/java/hackeru/edu/parsingxml/YnetFragment.java
+++ app/src/main/java/hackeru/edu/parsingxml/YnetFragment.java
@@ -1,6 +1,7 @@
 package hackeru.edu.parsingxml;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
@@ -18,12 +19,13 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class YnetFragment extends Fragment implements YnetDataSource.OnYnetArrivedListener {
-
+    RecyclerView rvYnet;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_ynet, container, false);
+        rvYnet = (RecyclerView) v.findViewById(R.id.rvYnet);
 
         YnetDataSource.getYnet(this);
 
@@ -38,7 +40,33 @@ public class YnetFragment extends Fragment implements YnetDataSource.OnYnetArriv
 
 
 
-    static class YnetAdapter {
+    static class YnetAdapter extends RecyclerView.Adapter<YnetAdapter.YnetViewHolder>{
+        //properties:
+        List<YnetDataSource.Ynet> data;
+        LayoutInflater inflater;
+        Context context;
+        //Constructor:
+        public YnetAdapter(Context context, List<YnetDataSource.Ynet> data) {
+            this.data = data;
+            this.context = context;
+            this.inflater = LayoutInflater.from(context); //Got the inflater from the Context.
+        }
+        @Override
+        public YnetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = inflater.inflate(R.layout.ynet_item, parent, false);
+            return new YnetViewHolder(v);
+        }
+        @Override
+        public void onBindViewHolder(YnetViewHolder holder, int position) {
+            YnetDataSource.Ynet ynet = data.get(position);
+            holder.tvTitle.setText(ynet.getTitle());
+            holder.tvDescription.setText(ynet.getContent());
+            //TODO: Get the images from the thumbnailUrl
+        }
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
 
         class YnetViewHolder extends RecyclerView.ViewHolder{
             TextView tvTitle;
